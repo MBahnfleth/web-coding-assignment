50dce90dd1c9deb6cc6973d055167517c680116b
simon <simon@protosimon.com>
2019-05-01 11:14:53 -0500

    chore: add react testing library

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import App from '../App';
+
+describe('<App />', () => {
+  it('should render hello world', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Hello World!')).toBeInTheDocument();
+  });
+});
