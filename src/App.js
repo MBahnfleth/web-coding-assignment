bbbc587aaba6068beb02beca3badc836ce00ebd7
simon <simon@protosimon.com>
2019-05-03 11:38:06 -0500

    chore: add docs about api key and axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 
+/**
+ * You can import your API_KEY into any file and use axios to make http requests.
+ * Read the axios docs for more details but you are welcome to swap out axios with whatever you like.
+ */
+
 function App() {
   return (
     <div className="app">
