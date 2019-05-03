5d4a82b483f283a0c472e3f201f4a6eee53181e4
simon <simon@protosimon.com>
2019-05-03 09:44:03 -0500

    chore: add API_KEY config

---
+++ src/config/apiKey.js
@@ -0,0 +1,9 @@
+/**
+ * Create a Marvel Developer Account
+ * The value of your public key should be assigned to API_KEY. 
+ * Don't worry; this file is git ignored and we will not know your API key.
+ */
+
+const API_KEY = '';
+
+export default API_KEY;
